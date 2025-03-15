4a40a4e9b03e71ff866a766b1a9607fb44c644ce
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-03-15 11:38:01 -0400

    Create and style CV canvas' section

---
+++ src/components/CanvasSection.jsx
@@ -0,0 +1,35 @@
+import "../styles/canvasSection.css";
+
+function CanvasSection({ title, items }) {
+	return (
+		<div className="canvasSection">
+			<h2 className="title">{title}</h2>
+			<div className="body">
+				{items.map((item) => {
+					if (item.isVisible) {
+						return (
+							<div className="item">
+								<div className="leftCol">
+									{item.startDate} - {item.endDate}
+									<br />
+									{item.location}
+								</div>
+								<div className="rightCol">
+									<h3 className="name">{item.name}</h3>
+									<p className="field">{item.field}</p>
+									{item.description && (
+										<p className="description">
+											{item.description}
+										</p>
+									)}
+								</div>
+							</div>
+						);
+					}
+				})}
+			</div>
+		</div>
+	);
+}
+
+export default CanvasSection;
