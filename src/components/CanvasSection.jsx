13f9255346b6c544a90819358cb9c8771ee88cb2
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-03-15 14:19:55 -0400

    Hide cv section if empty

--- src/components/CanvasSection.jsx
+++ src/components/CanvasSection.jsx
@@ -1,35 +1,37 @@
 import "../styles/canvasSection.css";
 
 function CanvasSection({ title, items }) {
-	return (
-		<div className="canvasSection">
-			<h2 className="title">{title}</h2>
-			<div className="body">
-				{items.map((item) => {
-					if (item.isVisible) {
-						return (
-							<div className="item">
-								<div className="leftCol">
-									{item.startDate} - {item.endDate}
-									<br />
-									{item.location}
+	if (items.length !== 0) {
+		return (
+			<div className="canvasSection">
+				<h2 className="title">{title}</h2>
+				<div className="body">
+					{items.map((item) => {
+						if (item.isVisible) {
+							return (
+								<div className="item">
+									<div className="leftCol">
+										{item.startDate} - {item.endDate}
+										<br />
+										{item.location}
+									</div>
+									<div className="rightCol">
+										<h3 className="name">{item.name}</h3>
+										<p className="field">{item.field}</p>
+										{item.description && (
+											<p className="description">
+												{item.description}
+											</p>
+										)}
+									</div>
 								</div>
-								<div className="rightCol">
-									<h3 className="name">{item.name}</h3>
-									<p className="field">{item.field}</p>
-									{item.description && (
-										<p className="description">
-											{item.description}
-										</p>
-									)}
-								</div>
-							</div>
-						);
-					}
-				})}
+							);
+						}
+					})}
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
 
 export default CanvasSection;
