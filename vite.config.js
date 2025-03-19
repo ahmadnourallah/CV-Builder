69dbf0ad243576c1171c4857d6c9b9a7508bd91e
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-03-19 15:24:13 -0400

    Use web worker to render pdf

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const reactPlugin = react();
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [reactPlugin],
+  worker: {
+    plugins: () => [reactPlugin],
+    format: "es",
+  }
 })
