adf495a1493c0adf9afa237f3e370a46bfb45e7d
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-03-27 11:42:15 -0400

    Add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 const reactPlugin = react();
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [reactPlugin],
-  worker: {
-    plugins: () => [reactPlugin],
-    format: "es",
-  }
-})
+	plugins: [reactPlugin],
+	worker: {
+		plugins: () => [reactPlugin],
+		format: "es",
+	},
+	base: "/CV-Builder/",
+});
